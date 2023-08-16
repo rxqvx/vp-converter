5b2f44ec3332d8d19cdd03b8218003e8a2d0dbd4
rxqvx <henriquekenji12@gmail.com>
2023-08-16 16:38:51 -0300

    feat: add first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer className="h-10 z-10 flex items-center justify-center shadow-2xl bg-gradient-to-r from-indigo-950 to-[#691F37] w-full">
+			<div className="text-slate-300">
+				Feito com ❤️ © 2023 Copyright Rique{" "}
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
