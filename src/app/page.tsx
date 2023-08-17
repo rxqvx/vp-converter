c4d30dc22367ca433953d204840ece968437498d
rxqvx <henriquekenji12@gmail.com>
2023-08-17 10:48:50 -0300

    feat: add show and hide message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import "./index.css";
+import { useState } from "react";
+
 import Image from "next/image";
 import Footer from "@/components/Footer";
 import VPConverter from "@/components/VPConverter";
 import ReaisConverter from "@/components/ReaisConverter";
 
 export default function Home() {
+	const [showMessage, setShowMessage] = useState<boolean>(true);
+
 	return (
 		<main className="flex relative w-full min-h-screen flex-col items-center justify-between">
 			<Image
@@ -28,31 +32,46 @@ export default function Home() {
 			</h1>
 			<div className=" p-24 flex justify-center items-center">
 				<div className="glassmorphism md:w-full md:max-w-full w-fit max-w-[300px]">
-					<div className="text-zinc-200 flex flex-col text-center">
-						<h2>Atenção</h2>
-						<p>
-							Os valores não podem ser determinados com precisão
-							devido à seguinte razão:
-						</p>
-						<p>
-							Não é viável estimar um valor exato devido à
-							natureza fixa e constante dos valores e bônus
-							envolvidos. Eles não seguem um padrão de aumento com
-							base em uma sequência numérica. Os cálculos empregam
-							médias e estimativas, considerando também as
-							flutuações nas taxas de câmbio, além das
-							discrepâncias nos bônus e custos associados a cada
-							pacote. Essa individualidade dos bônus conforme o
-							pacote é evidenciada por exemplos como 475 - sem
-							bônus, 1200 - 80 de bônus, 2300 - 190 de bônus, e
-							assim por diante.{" "}
-							<strong className="text-yellow-300">
-								Resumidamente, se o valor inserido não
-								corresponder ao valor dos pacotes, não será
-								aplicado nenhum bônus.
-							</strong>
-						</p>
-					</div>
+					{showMessage ? (
+						<div className="text-zinc-200 flex flex-col text-center">
+							<h2>Atenção</h2>
+							<p>
+								Os valores não podem ser determinados com
+								precisão devido à seguinte razão:
+							</p>
+							<p>
+								Não é viável estimar um valor exato devido à
+								natureza fixa e constante dos valores e bônus
+								envolvidos. Eles não seguem um padrão de aumento
+								com base em uma sequência numérica. Os cálculos
+								empregam médias e estimativas, considerando
+								também as flutuações nas taxas de câmbio, além
+								das discrepâncias nos bônus e custos associados
+								a cada pacote. Essa individualidade dos bônus
+								conforme o pacote é evidenciada por exemplos
+								como 475 - sem bônus, 1200 - 80 de bônus, 2300 -
+								190 de bônus, e assim por diante.{" "}
+								<strong className="text-yellow-300">
+									Resumidamente, se o valor inserido não
+									corresponder ao valor dos pacotes, não será
+									aplicado nenhum bônus.
+								</strong>
+								<button
+									onClick={() => setShowMessage(false)}
+									className="underline hover:text-slate-500"
+								>
+									(esconder mensagem)
+								</button>
+							</p>
+						</div>
+					) : (
+						<button
+							onClick={() => setShowMessage(true)}
+							className="flex justify-center items-center w-full text-zinc-200 underline hover:text-slate-500"
+						>
+							(mostrar mensagem)
+						</button>
+					)}
 					<div className="md:flex hidden flex-row items-center justify-evenly">
 						<VPConverter />
 						<ReaisConverter />
